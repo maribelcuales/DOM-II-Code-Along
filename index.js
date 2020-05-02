5f2e7c60b36363c77aac4626859a362649adc02b
maribelcuales <maribelcuales@gmail.com>
2020-05-02 20:27:27 +0800

    Added notes on events.

---
+++ index.js
@@ -0,0 +1,19 @@
+/////   DOM II  /////
+// By Grabriel Cabrejas 
+
+
+/* 
+// Chrome can make events: 
+- click, double click, scroll, load, etc. 
+
+// When a user interacts on a page, hundreds of events are being triggered. 
+
+// Project: DOM II
+- https://github.com/maribelcuales/DOM-II-Project
+
+// Events
+- When we click(e.g) the home nav link, there's just one target.  
+- The target is the innermost thing which we clicked 
+- NOTE: AN EVENT HAS ONE TARGET  
+*/ 
+
